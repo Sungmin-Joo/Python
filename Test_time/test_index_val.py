1dffbb5b2184eb9cc0623668d1a8fa177478ac2f
Sung-Min Joo <wntjdals2015@naver.com>
2019-04-22 22:19:27 +0900

    Desktop_update

---
+++ Test_time/test_index_val.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import time
+import random
+
+test_rand = [str(random.randrange(1,100)) for i in range(0,999999)]
+def Index_test():
+    temp = {}
+    for j in range(0,999999):
+        temp[test_rand[j]] = temp.get(test_rand[j],0) + 1
+    return temp
+
+def Str_test():
+    temp = {}
+    for j in test_rand:
+        temp[j] = temp.get(j,0) + 1
+    return temp    
+
+if __name__ == '__main__':
+    print('Func_called - main')
+    start_time = time.time() 
+    Index_test()
+    print('%0.5f' % (time.time() - start_time))
+    start_time = time.time() 
+    Str_test()
+    print('%0.5f' % (time.time() - start_time))
+else:
+    print('Func_called - imported')
+    
+'''
+---------- result ----------
+0.35960
+0.26090
+----------------------------
+'''
+    
